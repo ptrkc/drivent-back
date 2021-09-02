a78adfa48411a73e8a4ed7ea4c880caaf59d1697
Iuri Magnago <iurimp91@gmail.com>
2021-09-02 10:37:10 -0300

    feat: create ticket table migration

---
+++ src/migrations/1630589316515-CreateTicketEntity.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateTicketEntity1630589316515 implements MigrationInterface {
+    name = "CreateTicketEntity1630589316515"
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query("CREATE TABLE \"tickets\" (\"id\" SERIAL NOT NULL, \"name\" character varying NOT NULL, \"price\" integer NOT NULL, CONSTRAINT \"PK_343bc942ae261cf7a1377f48fd0\" PRIMARY KEY (\"id\"))");
+      await queryRunner.query("INSERT INTO \"tickets\" (name, price) VALUES ('Presencial', 250), ('Online', 100)");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query("DROP TABLE \"tickets\"");
+    }
+}
