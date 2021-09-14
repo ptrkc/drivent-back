ab1b4dd7e4a6bbef0bfa7464cf20be0bc471c9f2
Walteir Araujo <walteiraraujo@poli.ufrj.br>
2021-09-14 14:13:43 -0300

    fix: fix relations in Bookings table

--- src/migrations/1631636582591-CreateBookingsRelations.ts
+++ src/migrations/1631636582591-CreateBookingsRelations.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateRelationBookingRoomBookingHotel1631602969770 implements MigrationInterface {
-    name = "CreateRelationBookingRoomBookingHotel1631602969770"
+export class CreateBookingsRelations1631636582591 implements MigrationInterface {
+    name = "CreateBookingsRelations1631636582591"
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query("ALTER TABLE \"bookings\" ADD \"hotelId\" integer NOT NULL");
-      await queryRunner.query("ALTER TABLE \"bookings\" ADD \"roomId\" integer NOT NULL");
+      await queryRunner.query("ALTER TABLE \"bookings\" ADD \"hotelId\" integer ");
+      await queryRunner.query("ALTER TABLE \"bookings\" ADD \"roomId\" integer ");
       await queryRunner.query("ALTER TABLE \"bookings\" ADD CONSTRAINT \"FK_6713f297621b99988068dd63fe5\" FOREIGN KEY (\"hotelId\") REFERENCES \"hotel\"(\"id\") ON DELETE NO ACTION ON UPDATE NO ACTION");
       await queryRunner.query("ALTER TABLE \"bookings\" ADD CONSTRAINT \"FK_0172b36e4e054d6ebb819d58efb\" FOREIGN KEY (\"roomId\") REFERENCES \"rooms\"(\"id\") ON DELETE NO ACTION ON UPDATE NO ACTION");
     }
