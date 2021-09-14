d9fd4d5772fabc273722b5f7ebc2b8ffc2e326d8
Iuri Magnago <iurimp91@gmail.com>
2021-09-14 18:08:07 -0300

    feat: activitie entity, activitieAuditorium entity and relation activities-users

---
+++ src/entities/Activitie.ts
@@ -0,0 +1,29 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne, ManyToMany, JoinTable } from "typeorm";
+
+import ActivitieAuditorium from "./ActivitieAuditorium";
+import User from "./User";
+
+@Entity("activities")
+export default class RoomTypeHotel extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ type: "timestamp" })
+  startTime: Date;
+
+  @Column({ type: "timestamp" })
+  endTime: Date;
+
+  @Column()
+  vacancies: number;
+
+  @ManyToOne(() => ActivitieAuditorium, (activitieAuditorium: ActivitieAuditorium) => activitieAuditorium.activities)
+  activitieAuditorium: ActivitieAuditorium;
+
+  @ManyToMany(() => User)
+  @JoinTable()
+  users: [User];
+}
---
+++ src/entities/ActivitieAuditorium.ts
@@ -0,0 +1,15 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+
+import Activitie from "./Activitie";
+
+@Entity("activitiesAuditoriums")
+export default class RoomTypeHotel extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Activitie, (activities: Activitie) => activities.activitieAuditorium)
+  activities: [Activitie];
+}
