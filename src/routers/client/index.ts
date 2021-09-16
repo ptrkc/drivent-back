a2284f3b62b8b4cbab104f7dd800da6e3636d996
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-09-16 14:18:32 -0300

    fix: add token middleware in hotel route

--- src/routers/client/index.ts
+++ src/routers/client/index.ts
@@ -18,6 +18,6 @@ router.use("/auth", authRouter);
 router.use("/enrollments", tokenValidationMiddleware, enrollmentRouter);
 router.use("/tickets", ticketRouter);
 router.use("/booking", tokenValidationMiddleware, bookingRouter);
-router.use("/hotel", hotelRouter);
+router.use("/hotel", tokenValidationMiddleware, hotelRouter);
 
 export default router;
